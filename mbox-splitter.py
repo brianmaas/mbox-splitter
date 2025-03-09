9e48b0489c4aed4be885a61de589b526eb6ddac9
brianmaas <brian@maasfamily.com>
2025-03-09 09:26:04 -0700

    Processing feedback and memory optimization
    Added code for commandline feedback every 100 messages processed and better memory management so it doesn't crash on large files.

--- mbox-splitter.py
+++ mbox-splitter.py
@@ -1,74 +1,87 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import mailbox
 import sys
+import gc
 from os.path import exists
 
 if len(sys.argv) != 3:
-    print
-    print 'Usage: `python mbox-splitter.py filename.mbox size`'
-    print '         where `size` is a positive integer in Mb'
-    print
-    print 'Example: `python mbox-splitter.py inbox_test.mbox 50`'
-    print '         where inbox_test.mbox size is about 125 Mb'
-    print
-    print 'Result:'
-    print 'Created file `inbox_test_1.mbox`, size=43Mb, messages=35'
-    print 'Created file `inbox_test_2.mbox`, size=44Mb, messages=2'
-    print 'Created file `inbox_test_3.mbox`, size=30Mb, messages=73'
-    print 'Done'
-    exit()
-
+    print("\nUsage: `python mbox-splitter.py filename.mbox size`")
+    print("         where `size` is a positive integer in Mb\n")
+    print("Example: `python mbox-splitter.py inbox_test.mbox 50`")
+    print("         where inbox_test.mbox size is about 125 Mb\n")
+    print("Result:")
+    print("Created file `inbox_test_1.mbox`, size=43Mb, messages=35")
+    print("Created file `inbox_test_2.mbox`, size=44Mb, messages=2")
+    print("Created file `inbox_test_3.mbox`, size=30Mb, messages=73")
+    print("Done")
+    sys.exit()
 
 filename = sys.argv[1]
 if not exists(filename):
-    print 'File `{}` does not exist.'.format(filename)
-    exit()
+    print(f"File `{filename}` does not exist.")
+    sys.exit()
 
 try:
-    split_size = int(sys.argv[2])*1024*1024
+    split_size = int(sys.argv[2]) * 1024 * 1024  # Convert MB to bytes
 except ValueError:
-    print 'Size must be a positive number'
-    exit()
+    print("Size must be a positive number")
+    sys.exit()
 
 if split_size < 1:
-    print 'Size must be a positive number'
-    exit()
+    print("Size must be a positive number")
+    sys.exit()
 
-if mailbox.mbox(filename).__len__() == 0:
-    print 'Email messages in `{}` not found.'.format(filename)
-    exit()
-
+# Open the source mailbox once
+source_mbox = mailbox.mbox(filename)
+if len(source_mbox) == 0:
+    print(f"Email messages in `{filename}` not found.")
+    sys.exit()
 
 chunk_count = 1
-output = filename.replace('.mbox', '_' + str(chunk_count) + '.mbox')
+output = filename.replace('.mbox', f'_{chunk_count}.mbox')
 if exists(output):
-    print 'The file `{}` has already been splitted. Delete chunks to continue.'.format(filename)
-    exit()
+    print(f"The file `{filename}` has already been split. Delete chunks to continue.")
+    sys.exit()
 
-print 'Splitting `{}` into chunks of {}Mb ...\n'.format(filename, sys.argv[2])
+print(f"Splitting `{filename}` into chunks of {sys.argv[2]}Mb ...\n")
+
 total_size = 0
-of = mailbox.mbox(path=output, create=True)
-mc = 0
+of = mailbox.mbox(output, create=True)
+chunk_message_count = 0  # Count of messages in the current chunk
+global_count = 0         # Total messages processed
 
-for message in mailbox.mbox(filename):
-    mc += 1
-    message_size = str(message).__sizeof__()
+for message in source_mbox:
+    global_count += 1
+    chunk_message_count += 1
+
+    # Print an update every 100 messages
+    if global_count % 100 == 0:
+        print(f"Processed {global_count} messages so far...", end='\r')
+        sys.stdout.flush()
+        gc.collect()  # Force garbage collection to help with memory
+
+    message_bytes = message.as_bytes()
+    message_size = len(message_bytes)
 
     if total_size + message_size >= split_size:
         of.flush()
         of.close()
+        print(f"Created file `{output}`, size={total_size / (1024 * 1024):.2f}Mb, messages={chunk_message_count}.")
+
         chunk_count += 1
-        print 'Created file `{}`, size={}Mb, messages={}.'.format(output, total_size/1024/1024, mc)
         total_size = 0
-        output = filename.replace('.mbox', '_' + str(chunk_count) + '.mbox')
-        of = mailbox.mbox(path=output, create=True)
-        mc = 0
+        output = filename.replace('.mbox', f'_{chunk_count}.mbox')
+        of = mailbox.mbox(output, create=True)
+        chunk_message_count = 0
 
     of.add(message)
     total_size += message_size
 
-print 'Created file `{}`, size={}Mb, messages={}.'.format(output, total_size/1024/1024, mc)
+# Write out the last chunk
 of.flush()
 of.close()
-print '\nDone'
+print(f"Created file `{output}`, size={total_size / (1024 * 1024):.2f}Mb, messages={chunk_message_count}.")
+print(f"\nTotal messages processed: {global_count}")
+print("Done")
